46d1b590f46ba3bb7a754fde84072e63ad0448aa
alexey_sysoev <wowregfatgod@yandex.ru>
2021-05-07 12:57:50 +0300

    rm

--- src/JS/slider.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    $('.product__list').slick({
-        infinite: false,
-        prevArrow: $('.arrow--left'),
-        nextArrow: $('.arrow--right'),
-    });
-
-})
